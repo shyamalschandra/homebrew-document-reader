6256398951375bda648df8736dd95d43ca2e657c
Shyamal Chandra <sapanamicrosoftware@gmail.com>
2025-08-20 19:52:15 -0500

    Update Info.plist versions to 1.1.3

--- document-reader.rb
+++ document-reader.rb
@@ -40,9 +40,9 @@ class DocumentReader < Formula
           <key>CFBundleName</key>
           <string>Document Reader</string>
           <key>CFBundleVersion</key>
-          <string>1.1.1</string>
+          <string>1.1.3</string>
           <key>CFBundleShortVersionString</key>
-          <string>1.1.1</string>
+          <string>1.1.3</string>
           <key>CFBundlePackageType</key>
           <string>APPL</string>
           <key>CFBundleSignature</key>
